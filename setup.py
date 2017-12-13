f65dd853513d5c285531ac4ab89a31d466d7f2bd
YutoMizutani <Yuto>
2017-12-14 00:18:47 +0900

    5th commit

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='articleinfo',
+    name='articleinfo1',
     version='0.1.0',
     description='Get article information from your entered citation text.',
     long_description=readme,
     author='Yuto Mizutani',
     author_email='yuto.mizutani.dev@gmail.com',
     install_requires=[],
-    url='https://github.com/YutoMizutani/articleinfo',
+    url='https://github.com/YutoMizutani/articleinfo1',
     license=license,
     packages=find_packages(exclude=('tests', 'docs')),
     test_suite='tests',
-    scripts=['scripts/articleinfo'],
+    scripts=['scripts/articleinfo']
 )
